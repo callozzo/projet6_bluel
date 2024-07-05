56e29665dba7fd961c08a2f29672b90af0f94e6c
callozzo <killian.bouillon@gmail.com>
2024-07-05 11:36:37 +0200

    passage des projets en js

---
+++ FrontEnd/assets/categories.js
@@ -0,0 +1,5 @@
+const boutonTous = document.getElementById("tous");
+const boutonObjets = document.getElementById("objets");
+const boutonApparts = document.getElementById("appartements");
+const boutonHotels = document.getElementById("hotels");
+
--- FrontEnd/assets/main.js
+++ FrontEnd/assets/main.js
@@ -17,7 +17,31 @@ let categories = 'http://localhost:5678/api/categories'
 async function fetchData(urlFetchData) {
     const data = await project(urlFetchData);
     console.log(data);
+    return data;
 }
 
+const parentElement = document.querySelector(".gallery");
 
-fetchData(categories)
+async function retrieveData() {
+    try {
+        const data = await fetchData(img);
+        let array = data;
+        console.log(array);
+        for( i = 0; i < array.length; i++) {
+            const element = array[i];
+            const figure = document.createElement("figure");
+            parentElement.appendChild(figure);
+            figure.id = element.id;
+            const imageApi = document.createElement("img");
+            imageApi.src = element.imageUrl;
+            figure.appendChild(imageApi);
+            const textApi = document.createElement("figcaption");
+            textApi.innerHTML = element.title;
+            figure.appendChild(textApi);
+        }        
+    } catch (error) {
+        console.error("Erreur lors de la récupération des données :", error);
+    }
+}
+
+retrieveData();
