7d75ca486ebfa4b88aa9498cee462d093c92d509
callozzo <killian.bouillon@gmail.com>
2024-07-16 11:26:18 +0200

    filtres

--- FrontEnd/assets/categories.js
+++ FrontEnd/assets/categories.js
@@ -1,4 +1,48 @@
+// Récupération des boutons de filtres
 const boutonTous = document.getElementById("tous");
 const boutonObjets = document.getElementById("objets");
 const boutonApparts = document.getElementById("appartements");
 const boutonHotels = document.getElementById("hotels");
+const a = 1;
+const b = 2;
+const c = 3;
+
+// Fonction pour la récuperation de l'id des catégories pour les images
+async function retrieveCategory() {
+    try {
+        const data = await getImages();
+        for(let i = 0; i < data.length; i++) {
+            const element = data[i];
+            const categories = 0;
+            categories.id = element.category.id; 
+        }
+    } catch (error) {
+        console.error("Erreur lors de la création des éléments du DOM :", error);
+    }
+}
+
+// Fonction pour la la recuperation de l'id des catégories
+async function categories() {
+    try {
+        const elementId = await getCategories();
+        for(let i = 0; i < elementId.length; i++) {
+            const element = elementId[i];
+            const categoriesId = 0;
+            categoriesId.id = element.id;
+        }
+    } catch (error) {
+        console.error("Erreur lors de la création des éléments du DOM :", error);
+    }
+}
+
+// Fonction pour sa voir si les deux Id des API coïncide
+async function filtres() { 
+    if (categories() === retrieveCategory()) {
+        return 'correct';
+    } else {
+        return 'wrong';
+    }
+}
+
+const category = await categories()
+console.log(category);
--- FrontEnd/assets/main.js
+++ FrontEnd/assets/main.js
@@ -1,51 +1,54 @@
-async function project(url) {
+// URL des données
+let imgUrl = 'http://localhost:5678/api/works';
+let categorieUrl = 'http://localhost:5678/api/categories';
+
+// Sélection de l'élément parent dans le DOM
+const parentElement = document.querySelector(".gallery");
+
+// Fonction pour récupérer les données à partir d'une URL
+async function fetchData(url) {
     try {
         const response = await fetch(url);
         if (!response.ok){
             throw new Error(`status error : ${response.status}`)
         }
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error("voici l'erreur :", error);
-    }
-}
-
-let imgUrl = 'http://localhost:5678/api/works'
-let categorie = 'http://localhost:5678/api/categories'
-
-async function fetchData(urlFetchData) {
-    const data = await project(urlFetchData);
-    console.log(data);
-    return data;
-}
-
-const imagesData = await fetchData(imgUrl);
-
-console.log(imagesData);
-
-const parentElement = document.querySelector(".gallery");
-
-async function retrieveData() {
-    try {
-        const data = await fetchData(imgUrl);
-        let array = data;
-        console.log(array);
-        for( i = 0; i < array.length; i++) {
-            const element = array[i];
-            const figure = document.createElement("figure");
-            parentElement.appendChild(figure);
-            figure.id = element.id;
-            const imageApi = document.createElement("img");
-            imageApi.src = element.imageUrl;
-            figure.appendChild(imageApi);
-            const textApi = document.createElement("figcaption");
-            textApi.innerHTML = element.title;
-            figure.appendChild(textApi);
-        }        
+        return await response.json();
     } catch (error) {
         console.error("Erreur lors de la récupération des données :", error);
     }
 }
 
+// Fonction pour récupérer les images
+async function getImages() {
+    return await fetchData(imgUrl);
+}
+
+//Fonction pour récupérer les catégories
+async function getCategories() {
+    return await fetchData(categorieUrl);
+}
+
+// Fonction pour récupérer les données et créer les éléments du DOM
+async function retrieveData() {
+    try {
+        const data = await getImages();
+        for(let element of data) {
+            const figure = document.createElement("figure");
+            parentElement.appendChild(figure);
+            figure.id = element.id;
+
+            const imageApi = document.createElement("img");
+            imageApi.src = element.imageUrl;
+            figure.appendChild(imageApi);
+
+            const textApi = document.createElement("figcaption");
+            textApi.innerHTML = element.title;
+            figure.appendChild(textApi); 
+        }        
+    } catch (error) {
+        console.error("Erreur lors de la création des éléments du DOM :", error);
+    }
+}
+
+// Appel de la fonction principale
 retrieveData();
