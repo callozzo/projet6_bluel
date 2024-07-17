80f4e93061f6ed20c1ea4be0148e1d1ba426361b
callozzo <killian.bouillon@gmail.com>
2024-07-17 12:03:13 +0200

    test filtres js

--- FrontEnd/assets/categories.js
+++ FrontEnd/assets/categories.js
@@ -1,48 +1,59 @@
-// Récupération des boutons de filtres
+// Récupération des boutons des filtres
 const boutonTous = document.getElementById("tous");
 const boutonObjets = document.getElementById("objets");
 const boutonApparts = document.getElementById("appartements");
 const boutonHotels = document.getElementById("hotels");
-const a = 1;
-const b = 2;
-const c = 3;
 
-// Fonction pour la récuperation de l'id des catégories pour les images
+// Fonction pour la récupération de l'id des catégories pour les images
 async function retrieveCategory() {
     try {
         const data = await getImages();
+        const categories = [];
         for(let i = 0; i < data.length; i++) {
             const element = data[i];
-            const categories = 0;
-            categories.id = element.category.id; 
+            categories.push(element.category.id); 
         }
+        return categories;
     } catch (error) {
         console.error("Erreur lors de la création des éléments du DOM :", error);
+        return [];
     }
 }
 
-// Fonction pour la la recuperation de l'id des catégories
+// Fonction pour la récupération de l'id des catégories
 async function categories() {
     try {
-        const elementId = await getCategories();
-        for(let i = 0; i < elementId.length; i++) {
-            const element = elementId[i];
-            const categoriesId = 0;
-            categoriesId.id = element.id;
+        const data = await getCategories();
+        const categoriesId = [];
+        for(let i = 0; i < data.length; i++) {
+            const element = data[i];
+            categoriesId.push(element.id);
         }
+        return categoriesId;
     } catch (error) {
         console.error("Erreur lors de la création des éléments du DOM :", error);
+        return [];
     }
 }
 
-// Fonction pour sa voir si les deux Id des API coïncide
-async function filtres() { 
-    if (categories() === retrieveCategory()) {
-        return 'correct';
-    } else {
-        return 'wrong';
-    }
-}
+// Fonction pour savoir si les deux Id des API coïncident
+async function filtres(value) { 
+    
+    const categoryIds = await categories();
+    const imageCategoryIds = await retrieveCategory();
 
-const category = await categories()
-console.log(category);
+    console.log("Category IDs:", categoryIds);
+    console.log("Image Category IDs:", imageCategoryIds);
+
+    const commonIds = imageCategoryIds.filter(id => categoryIds.includes(id) && id === value);
+    const newArray = [...new Set(commonIds)];
+    return newArray
+    }
+
+boutonObjets.addEventListener('click', () => {
+    filtres(1).then(commonIds => {
+        console.log('voici les ids en commun :', commonIds);
+    })
+})
+
+filtres(2).then(result => console.log(result));
